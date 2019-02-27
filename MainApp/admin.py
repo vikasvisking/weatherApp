5562e086b50e4ae710975eebb22aee6da5f1320c
Vikas Passi <vikasvisking007@gmail.com>
2019-02-27 11:19:03 +0530

    add models to admin

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from MainApp.models import *
 
 # Register your models here.
+
+# add models to admin pannel
+admin.site.register(Metrics)
+admin.site.register(MetricsValues)
